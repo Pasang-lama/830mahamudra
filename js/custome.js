b812166296ab9920a3caa35b6686b5162efd8a5b
Pasang-lama <lpasang182@gmail.com>
2024-07-11 07:58:40 +0545

    works on the video section

--- js/custome.js
+++ js/custome.js
@@ -7,3 +7,25 @@ window.addEventListener("scroll", function () {
         header.classList.remove("scrolled")
     }
 })
+
+
+$(document).ready(function () {
+    $('.tour-destination-slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+    });
+  });
+
+  $(function() {
+    $('.popup-youtube, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
